fc94f7602e49097c2ce95e815f99e4d0c948f7f1
Nader Ashraf <nadermamdouh977@gmail.com>
2026-05-14 20:33:00 +0300

    create basic bank acc class

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from module.bank_class import *
 
-Nader = bank_account()
+Nader = bank_account("Nader", 5000)
+Nader.get_balance()
+
+Nader.deposit(5000)
+Nader.withdraw(2000)
--- module/bank_class.py
+++ module/bank_class.py
@@ -1,2 +1,24 @@
 class bank_account:
-    pass
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance
+
+    def get_balance(self):
+        print(f"{self.name} have $ {self.__balance:.2f}.")
+
+    def deposit(self, deposit):
+        if deposit:
+            self.__balance = self.__balance + deposit
+            print(f"You successfully deposit $ {deposit:.2f}.")
+            self.get_balance()
+        else:
+            print("plaese Enter a positive value")
+
+    def withdraw(self, withdraw):
+        if (self.__balance - withdraw) >= 0:
+            print(f"you successfuly withdraw $ {withdraw:.2f}. ")
+            self.__balance = self.__balance-withdraw
+            self.get_balance()
+        else:
+            print("You don't have $ {withdraw:.2f}")
+            self.get_balance()
